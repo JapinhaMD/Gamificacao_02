34660fe762f5538868e8e65d0f0c6431c11a1ef8
JapinhaMD <105886490+JapinhaMD@users.noreply.github.com>
2023-05-02 10:10:22 -0300

    Add files via upload

---
+++ Gamificação 02/Gamificação 02/Camisseta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamificação_02
+{
+   class Camisetas
+    {
+   
+        
+    }
+}
---
+++ Gamificação 02/Gamificação 02/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamificação_02
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public CategoriaProduto Categoria { get; set; }
+    }
+
+
+}
