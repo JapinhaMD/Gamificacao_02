9571bb249f8548ee28cbdeb2b3b373a926d84133
JapinhaMD <105886490+JapinhaMD@users.noreply.github.com>
2023-05-04 14:34:32 -0300

    Add files via upload

---
+++ Gamificacao/Gamificação 02/Gamificação 02/Model/Acessorio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamificacao_02
+{
+    abstract class Acessorio : Produto
+    {
+        public string Tamanho { get; set; }
+        public string Cor { get; set; }
+    }
+
+    class Sapato : Acessorio
+    {
+        public string Material { get; set; }
+    }
+
+
+    class Bolsa : Acessorio
+    {
+        public int Compartimentos { get; set; }
+    }
+
+}
