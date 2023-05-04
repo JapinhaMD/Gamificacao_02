22275457c960df2bcb41ec797b150a04f91fed57
JapinhaMD <105886490+JapinhaMD@users.noreply.github.com>
2023-05-04 14:36:09 -0300

    Add files via upload

---
+++ Gamificacao/Gamificação 02/Gamificação 02/Produtos/Calca.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamificacao_02
+{
+
+  class Calcas
+	{
+		public string Calca(CarrinhoDeCompras carrinho)
+		{
+            string opcaoCalca = Console.ReadLine();
+            switch (opcaoCalca)
+            {
+                case "1":
+                    Produto calcaJeans = new Produto
+                    { 
+                     Nome = "Calca Jeans",
+                     Preco = 120.0,
+                     Categoria = CategoriaProduto.Calca
+                    };
+
+                    carrinho.AdicionarProduto(calcaJeans);
+                    Console.WriteLine("Calca Jeans adicionada ao carrinho de compras!");
+                    Console.WriteLine();
+                    break;
+
+                case "2":
+                    Produto calcaSocial = new Produto
+                    { 
+                     Nome = "Calca Social",
+                     Preco = 150.0, 
+                     Categoria = CategoriaProduto.Calca
+                    };
+
+                    carrinho.AdicionarProduto(calcaSocial);
+                    Console.WriteLine("Calca Social adicionada ao carrinho de compras!");
+                    Console.WriteLine();
+                    break;
+
+                case "3":
+                    Console.WriteLine("|Promoções para Calcas|");
+                    Console.WriteLine("20% de desconto para todas as Calcas");
+                    break;
+
+                case "0":
+                    break;
+            }
+                return opcaoCalca;
+        }
+	}
+}
---
+++ Gamificacao/Gamificação 02/Gamificação 02/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Gamificacao_02;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+class Program
+{
+
+
+    static void Main(string[] args)
+    {
+
+        Menu menu = new Menu();
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        Camisetas camiseta = new Camisetas();
+        Calcas calca = new Calcas();
+        Sapatos sapato = new Sapatos();
+        Bolsas bolsa = new Bolsas();
+       
+
+        bool continuar = true;
+        do
+        {
+
+            menu.MenuPrincipal();
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+
+                    menu.MenuCamiseta();
+                    camiseta.Camiseta(carrinho);
+
+                    break;
+                    
+
+                case "2":
+
+                    menu.MenuCalca();
+                    calca.Calca(carrinho);
+                  
+                    break;
+
+
+                case "3":
+
+                    menu.MenuSapato();
+                    sapato.Sapato(carrinho);
+
+                    break;
+
+
+                case "4":
+
+                    menu.MenuBolsa();
+                    bolsa.Bolsa(carrinho);
+                           
+                    break;
+
+
+                case "5":
+                    carrinho.MostrarCarrinho();
+                    break;
+                case "0":
+                    continuar = false;
+                    break;
+            }
+        } while (continuar);
+
+
+        double totalCompra = carrinho.CalcularValorTotal();
+        Console.WriteLine($"Valor total da compra: R${totalCompra}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
